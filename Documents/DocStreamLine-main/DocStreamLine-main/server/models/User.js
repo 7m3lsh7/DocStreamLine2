9e43d0a9d80b0f96bf1207c027830004302c26db
7m3lsh7 <felopater.remon2020@gmail.com>
2025-09-16 06:59:50 +0300

    first

---
+++ Documents/DocStreamLine-main/DocStreamLine-main/server/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    // --- Core Identifiers ---
+    _id: { type: String, required: true },
+    username: { type: String, unique: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    // --- Profile Details ---
+    full_name: { type: String, required: true },
+    bio: { type: String, default: "Hey Doctors!" },
+    profile_picture: { type: String, default: "" },
+    cover_photo: { type: String, default: "" },
+    location: { type: String, default: "" },
+    // --- Social & Connections ---
+    followers: [{ type: String, ref: "User" }],
+    following: [{ type: String, ref: "User" }],
+    connections: [{ type: String, ref: "User" }],
+  },
+  { timestamps: true, minimize: false }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
